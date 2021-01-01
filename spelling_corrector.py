f2d40647f438f8b18b904388b4594bbbfbee06e9
mustafaKus <m.kus@hotmail.com>
2021-01-01 09:04:01 +0300

    Symetric delete spelling correction algorithm is added

--- spelling_corrector.py
+++ spelling_corrector.py
@@ -3,12 +3,13 @@
 
 from itertools import chain
 from string import ascii_lowercase, digits, punctuation
+from abc import ABC, abstractmethod
+from collections import defaultdict
 
 
-class SpellingCorrector:
+class AbstractSpellingCorrector(ABC):
 
-    """Implements the spell checking with Peter Norvig's approach with more than one dictionary
-    see the blog post here https://norvig.com/spell-correct.html"""
+    """Implements the spelling corrector interface"""
 
     _CHARACTERS = ascii_lowercase + digits + punctuation + " "
 
@@ -17,14 +18,9 @@ class SpellingCorrector:
         self._tokenizer = tokenizer or (lambda utterance: utterance.split())
         self._retokenizer = retokenizer or (lambda tokens: " ".join(tokens))
 
+    @abstractmethod
     def _candidates(self, token):
         """Returns candidate words from the dictionaries by looking at the edit distances"""
-        token_as_list = [token]
-        token_1_edits = SpellingCorrector._one_edit_token_distances(token)
-        token_2_edits = SpellingCorrector._two_edits_token_distances(token)
-        return (
-            self._known_in(token_as_list) or self._known_in(token_1_edits) or self._known_in(token_2_edits) or
-            token_as_list)
 
     def _correct_token(self, token):
         """Corrects tokens"""
@@ -38,6 +34,28 @@ class SpellingCorrector:
             return 0
         return frequency_value_of_word
 
+    def correct(self, utterance):
+        """Tokenizes the utterance and corrects each token"""
+        tokens = self._tokenizer(utterance.lower())
+        corrected_tokens = [self._correct_token(token) for token in tokens]
+        return self._retokenizer(corrected_tokens)
+
+
+class NorvigCorrector(AbstractSpellingCorrector):
+
+    """Implements the spell checking with Peter Norvig's approach see the blog post here
+    https://norvig.com/spell-correct.html"""
+
+
+    def _candidates(self, token):
+        """Returns candidate words from the dictionaries by looking at the edit distances"""
+        token_as_list = [token]
+        token_1_edits = NorvigCorrector._one_edit_token_distances(token)
+        token_2_edits = NorvigCorrector._two_edits_token_distances(token)
+        return (
+            self._known_in(token_as_list) or self._known_in(token_1_edits) or self._known_in(token_2_edits) or
+            token_as_list)
+
     def _known_in(self, words):
         """Returns the subset of words that appear in the vocabulary"""
         return set(word for word in words if self._word_2_frequency.get(word))
@@ -50,10 +68,10 @@ class SpellingCorrector:
             left_split + right_split[1:] for left_split, right_split in splitted_token_pairs if right_split)
         inserted_variations = (
             left_split + insert_candidate + right_split for left_split, right_split in splitted_token_pairs for
-            insert_candidate in SpellingCorrector._CHARACTERS)
+            insert_candidate in NorvigCorrector._CHARACTERS)
         replaced_variations = (
             left_split + replace_candidate + right_split[1:] for left_split, right_split in splitted_token_pairs if
-            right_split for replace_candidate in SpellingCorrector._CHARACTERS)
+            right_split for replace_candidate in NorvigCorrector._CHARACTERS)
         transposed_variations = (
             left_split + right_split[1] + right_split[0] + right_split[2:] for left_split, right_split in
             splitted_token_pairs if len(right_split) > 1)
@@ -62,13 +80,59 @@ class SpellingCorrector:
     @staticmethod
     def _two_edits_token_distances(token):
         """Returns the two edit distances of the token"""
-        return [
-            two_edits_distance_of_word for one_edit_distance_of_word in SpellingCorrector._one_edit_token_distances(
-                token) for two_edits_distance_of_word in SpellingCorrector._one_edit_token_distances(
-                    one_edit_distance_of_word)]
+        return (
+            two_edits_distance_of_word for one_edit_distance_of_word in NorvigCorrector._one_edit_token_distances(
+                token) for two_edits_distance_of_word in NorvigCorrector._one_edit_token_distances(
+                    one_edit_distance_of_word))
 
-    def correct(self, utterance):
-        """Tokenizes the utterance and corrects each token"""
-        tokens = self._tokenizer(utterance.lower())
-        corrected_tokens = [self._correct_token(token) for token in tokens]
-        return self._retokenizer(corrected_tokens)
+
+class SymmetricDeleteCorrector(AbstractSpellingCorrector):
+
+    """Implements the spell checking with the symmetric delete spelling correction algorithm see
+    https://wolfgarbe.medium.com/1000x-faster-spelling-correction-algorithm-2012-8701fcd87a5f"""
+
+    def __init__(self, word_2_frequency, retokenizer=None, tokenizer=None):
+        super(SymmetricDeleteCorrector, self).__init__(word_2_frequency, retokenizer, tokenizer)
+        self._deleted_variation_2_dictionary_words = self._create_deleted_variation_2_dictionary_words()
+
+    def _candidates(self, token):
+        """Returns candidate words from the dictionaries by looking at the edit distances"""
+        token_1_edits = SymmetricDeleteCorrector._one_edit_deleted_variations(token)
+        token_2_edits = SymmetricDeleteCorrector._one_edit_deleted_variations(token)
+        return (
+            self._deleted_variation_2_dictionary_words[token] or
+            (
+                set().union(
+                    chain(
+                        self._deleted_variation_2_dictionary_words[token_1_edit] for token_1_edit in token_1_edits))) or
+            (
+                set().union(
+                    chain(self._deleted_variation_2_dictionary_words[token_2_edit] for token_2_edit in token_2_edits))))
+
+    def _create_deleted_variation_2_dictionary_words(self):
+        """Creates the deleted variation to dictionary words"""
+        deleted_variation_2_dictionary_words = defaultdict(set)
+        for word in self._word_2_frequency.keys():
+            deleted_variations = chain(self._one_edit_deleted_variations(word), self._two_edit_deleted_variations(word))
+            for deleted_variation in deleted_variations:
+                deleted_variation_2_dictionary_words[deleted_variation].add(word)
+        return deleted_variation_2_dictionary_words
+
+    @staticmethod
+    def _one_edit_deleted_variations(token):
+        """Returns the one edit deleted variations of the token"""
+        splitted_token_pairs = [(token[:i], token[i:]) for i in range(len(token) + 1)]
+        return (left_split + right_split[1:] for left_split, right_split in splitted_token_pairs if right_split)
+
+    @staticmethod
+    def _two_edit_deleted_variations(token):
+        """Returns the two edit deleted variations of the token"""
+        return (
+            two_edits_distance_of_word for one_edit_distance_of_word in
+            SymmetricDeleteCorrector._one_edit_deleted_variations(token) for two_edits_distance_of_word in
+            SymmetricDeleteCorrector._one_edit_deleted_variations(one_edit_distance_of_word))
+
+
+
+
+
--- test_spelling_corrector.py
+++ test_spelling_corrector.py
@@ -7,16 +7,17 @@ import sys
 import unittest
 from unittest import TestCase
 
-from spelling_corrector import SpellingCorrector
+from spelling_corrector import NorvigCorrector, SymmetricDeleteCorrector
 
 
 class SpellingCorrectorTest(TestCase):
     """Implements the test class for the spelling corrector"""
 
-    def test_spelling_corrector(self):
-        """Tests the spelling corrector"""
+    def test_norvig_corrector(self):
+        """Tests the norvig corrector"""
         current_working_directory = os.path.abspath(os.getcwd())
         tests_directory = os.path.join(current_working_directory, "tests")
+        logging.info("Tests the norvig corrector")
         logging.info("Tests directory is %s" % tests_directory)
         for test_directory_name in os.listdir(tests_directory):
             logging.info("Testing in %s directory" % test_directory_name)
@@ -30,7 +31,33 @@ class SpellingCorrectorTest(TestCase):
                 for _, line in enumerate(dictionary_lines):
                     word, frequency_value = line.strip().split()
                     word_2_frequency[word.lower()] = int(frequency_value)
-            spelling_corrector = SpellingCorrector(word_2_frequency)
+            spelling_corrector = NorvigCorrector(word_2_frequency)
+            with open(test_input_2_expected_output_path) as input_2_expected_output_file:
+                logging.info("Reading the test data")
+                input_2_expected_output = json.load(input_2_expected_output_file)
+                for input_, expected_output in input_2_expected_output.items():
+                    logging.info("Expected output for the input '%s' is '%s'" % (input_, expected_output))
+                    self.assertEqual(expected_output, spelling_corrector.correct(input_))
+
+    def test_symmetric_delete_corrector(self):
+        """Tests the symmetric delete corrector"""
+        current_working_directory = os.path.abspath(os.getcwd())
+        tests_directory = os.path.join(current_working_directory, "tests")
+        logging.info("Tests the norvig corrector")
+        logging.info("Tests directory is %s" % tests_directory)
+        for test_directory_name in os.listdir(tests_directory):
+            logging.info("Testing in %s directory" % test_directory_name)
+            test_directory_path = os.path.join(tests_directory, test_directory_name)
+            dictionary_path = os.path.join(test_directory_path, "dictionary.txt")
+            test_input_2_expected_output_path = os.path.join(test_directory_path, "input_2_expected_output.json")
+            word_2_frequency = {}
+            with open(dictionary_path, "r") as dictionary_file:
+                logging.info("Reading the dictionary %s" % test_directory_name)
+                dictionary_lines = dictionary_file.readlines()
+                for _, line in enumerate(dictionary_lines):
+                    word, frequency_value = line.strip().split()
+                    word_2_frequency[word.lower()] = int(frequency_value)
+            spelling_corrector = SymmetricDeleteCorrector(word_2_frequency)
             with open(test_input_2_expected_output_path) as input_2_expected_output_file:
                 logging.info("Reading the test data")
                 input_2_expected_output = json.load(input_2_expected_output_file)
